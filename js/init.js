f78de8cac8241e00131c271f115d500d3a75b694
tprasadtp <code@prasadt.com>
2018-03-29 05:00:37 +0200

    base-template redesign

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-trigger").dropdown();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.tooltipped').tooltip();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
